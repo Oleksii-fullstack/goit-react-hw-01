86db547535ff7ccb2d7214ccc669c5bf513735cc
Oleksii Yakovliev <aleksoyak@gmail.com>
2025-05-26 17:13:04 +0300

    update Profile jsx file and done task-1 there

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,35 @@
+const Profile = ({
+  avatar,
+  username,
+  tag,
+  location,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <>
+      <div>
+        <img src={avatar} alt={username} />
+        <p>{username}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{likes}</span>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Profile;
