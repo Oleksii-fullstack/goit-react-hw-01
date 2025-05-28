1b1d5b516f120bda8d9550345cc66d8497f1fe60
Oleksii Yakovliev <aleksoyak@gmail.com>
2025-05-28 11:30:30 +0300

    update TransactionHistory styles and done all tasks

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,23 +3,23 @@ import s from "./TransactionHistory.module.css";
 const TransactionHistory = ({ items }) => {
   return (
     <>
-      <table>
-        <thead>
+      <table className={s.table}>
+        <thead className={s.tableHead}>
           <tr>
             <th>Type</th>
             <th>Amount</th>
             <th>Currency</th>
           </tr>
         </thead>
-        {items.map((transaction) => (
-          <tbody key={transaction.id}>
-            <tr>
+        <tbody>
+          {items.map((transaction) => (
+            <tr key={transaction.id}>
               <td>{transaction.type}</td>
               <td>{transaction.amount}</td>
               <td>{transaction.currency}</td>
             </tr>
-          </tbody>
-        ))}
+          ))}
+        </tbody>
       </table>
     </>
   );
